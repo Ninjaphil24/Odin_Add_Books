c470a8706214dde529ba2a06c1b6f342c88dab0a
Ninjaphil24 <mphilippos@hotmail.com>
2022-02-23 17:36:13 +0200

    Submit to url works

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+if (document.readyState == 'loading') {
+	document.addEventListener('DOMContentLoaded', ready)
+} else {
+	ready()
+}
+
+function ready() {
+	var title = new URL(window.location).searchParams.get('title')
+	document.getElementById('title-input').value = title
+	var author = new URL(window.location).searchParams.get('author')
+	document.getElementById('author-input').value = author
+	var pages = new URL(window.location).searchParams.get('pages')
+	document.getElementById('pages-input').value = pages
+	var readYN = new URL(window.location).searchParams.get('readYN')
+	document.getElementById('readYN-input').value = readYN
+}
+// document.getElementById('query-output').innerHTML = query
+
+
 function Book(title,author,pages,read) {
     this.title=title;
     this.author=author;
