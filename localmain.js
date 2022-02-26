a4a6f96a08badd2a9722ee2beb1873df87682a05
Ninjaphil24 <mphilippos@hotmail.com>
2022-02-26 18:57:16 +0200

    Add output. Array push not working properly

--- localmain.js
+++ localmain.js
@@ -5,6 +5,7 @@ const inpPages = document.getElementById('pages-input')
 const inpReadYN = document.getElementById('readYN-input')
 const insertBtn = document.getElementById('insert')
 const Books = localStorage.key(0)
+
 function Book(title,author,pages,read) {
     this.title=title;
     this.author=author;
@@ -20,8 +21,8 @@ let libraryParser = () => {
     else return null
 }
 
+libraryParser()
 function insert() {
-    libraryParser()
     const title = inpTitle.value
     const author = inpAuthor.value
     const pages = inpPages.value
@@ -31,4 +32,28 @@ function insert() {
     location.reload()      
 }
 
-// Create output from parsed array, then creae a delete button of any given output
+function printBook() {
+    const card = document.getElementById('card')
+    // for(let i=0;i<myLibrary.length;i++) 
+    for (let i in myLibrary)
+    {
+        const div = document.createElement('div')
+        div.textContent = "Title: "+ myLibrary[i].title 
+        const div2 = document.createElement('div')
+        div2.textContent = "Author: "+ myLibrary[i].author 
+        const div3 = document.createElement('div')
+        div3.textContent = "Pages: "+ myLibrary[i].pages
+        const div4 = document.createElement('div')
+        div4.textContent = "Have you read this book: "+ myLibrary[i].read
+        const br = document.createElement('br')
+        
+        myLibrary[i].author
+        card.appendChild(div)    
+        card.appendChild(div2)    
+        card.appendChild(div3)    
+        card.appendChild(div4)    
+        card.appendChild(br)    
+    }
+}
+
+printBook()
