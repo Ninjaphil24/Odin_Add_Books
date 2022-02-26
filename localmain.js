8b34c6e8befc98e60158da6c1b3153feba647ded
Ninjaphil24 <mphilippos@hotmail.com>
2022-02-26 11:24:46 +0200

    Local Storage Array Successful

--- localmain.js
+++ localmain.js
@@ -1,3 +1,4 @@
+let myLibrary = []
 const inpTitle = document.getElementById('title-input')
 const inpAuthor = document.getElementById('author-input')
 const inpPages = document.getElementById('pages-input')
@@ -13,12 +14,30 @@ function Book(title,author,pages,read) {
 
 insertBtn.addEventListener('click', insert)
 
+let libraryParser = () => {
+    if(localStorage.getItem(myLibrary))return JSON.parse(localStorage.getItem(myLibrary))
+    else return null
+}
+
 function insert() {
+    libraryParser()
     const title = inpTitle.value
     const author = inpAuthor.value
     const pages = inpPages.value
     const read = inpReadYN.value
-    const insertBook = JSON.stringify(new Book(title,author,pages,read))
-    localStorage.setItem("Book",insertBook)
-        
+    myLibrary.push(new Book(title,author,pages,read))
+    localStorage.setItem("Book",JSON.stringify(myLibrary))        
 }
+// function insert() {
+//     libraryParser()
+//     const title = "Potter"
+//     const author = "Rowling"
+//     const pages = 255
+//     const read = "Yes"
+    
+// }
+
+
+
+
+// You must create an array.  Use Stringify to save the array in local storage.  Use parse to to the array back to an array to add stuff.  Use Stringify to resave the array in local storate.
